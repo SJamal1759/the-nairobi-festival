a3a9603c7a22c07bad09e3dc6e039195066d464a
Jamal Sikuku <sjamal1759@gmail.com>
2023-12-26 10:56:17 +0300

    background image fixing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react'
 import './resources/styles.css'
 import Header from './components/header_footer/Header';
-import { Height } from '@mui/icons-material';
+// import { Height } from '@mui/icons-material';
 import Featured from './components/featured';
-import Carrousel from './components/featured/Carrousel';
+import Carrousel from './components/featured/Carrousel'
 
 class App extends Component {
   render() {
     return (
-      <div className='App' style={{height:'1500px',background:'cornflowerblue'}}>
+      <div className='App' style={{height:'1500px'}}>
         <Header/>
         <Featured/>
+        <Carrousel/>
       </div>
     )
   }
 }
-export default App;
+export default App; 
--- src/components/featured/index.js
+++ src/components/featured/index.js
@@ -1,23 +1,17 @@
 import React from 'react';
 import Carrousel from './Carrousel';
 
-
 const Featured = () => {
-    return (
-        <div style={{position:'relative'}}>
-
+    return(
+        <div className="featured_container">
             <Carrousel/>
-
             <div className="artist_name">
                 <div className="wrapper">
                     Ariana Grande
                 </div>
             </div>
-
-            
-
         </div>
-    );
-};
+    )
+}
 
 export default Featured;
