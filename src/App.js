059b0df71caaa3d54816ee5f61c50f802aaccd1b
Jamal Sikuku <sjamal1759@gmail.com>
2024-02-02 08:13:54 +0300

    addition of discount and highlight page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Footer from "./components/header_footer/Footer";
 // import { Height } from '@mui/icons-material';
 import Featured from "./components/featured";
 import VenueInfo from "./components/venueInfo";
+import Highlights from "./components/highlights/index";
 
 class App extends Component {
   render() {
@@ -14,6 +15,8 @@ class App extends Component {
         <Header />
         <Featured />
         <VenueInfo />
+        <Highlights />
+
         <div style={{ backgroundColor: "red", height: "800px" }}></div>
         <div style={{ backgroundColor: "blue", height: "800px" }}></div>
         <div style={{ backgroundColor: "green", height: "800px" }}></div>
